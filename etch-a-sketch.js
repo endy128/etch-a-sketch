4110078f010348ee8168ca4c78ac22b22cf37f03
Neil Douglas <endy64@icloud.com>
2022-03-22 18:10:16 +0000

    added button to reset the box added prompt to allow user to set the number of pixels per side added max of 100 pixels per side

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,21 +1,48 @@
 const container = document.querySelector('.container');
 const numOfSquares = 0;
 
-function drawBoxes(numOfSquares) {
+function drawBoxes(numOfSquares, width) {
     for (let i = 0; i < numOfSquares; i++) {
         const newBox = document.createElement('div');
-        // newBox.setAttribute('style', 'background-color: pink;');
+        newBox.style.width =  `${width}px`;
+        newBox.style.height = `${width}px`;
         newBox.classList.add('pixel');
         container.appendChild(newBox);
     }
 }
 
-drawBoxes(576);
+
+function addHoverListener() {
+    const pixels = Array.from(document.querySelectorAll('.pixel'));
+    pixels.forEach(pixel => pixel.addEventListener('mouseover', () => {
+        pixel.classList.add('activated');  // change the class of the pixel to change the bg color
+        // pixel.setAttribute('style', 'background-color: black;');
+    }));
+}
 
 
-const pixels = Array.from(document.querySelectorAll('.pixel'));
-pixels.forEach(pixel => pixel.addEventListener('mouseover', () => {
-    pixel.classList.add('activated');
-    // pixel.setAttribute('style', 'background-color: black;');
-}));
+
+const button = document.querySelector('.reset-button');
+button.addEventListener('click', () => {
+    const pixels = Array.from(document.querySelectorAll('.pixel'));
+    pixels.forEach(pixel => pixel.classList.remove('activated'));
+    let numBoxes = window.prompt('Please enter the number of squares per side: ', '26');
+    if (numBoxes > 100) {
+        numBoxes = 100;
+    }
+    deleteBoxes(container);
+    drawBoxes((numBoxes * numBoxes), (624 / numBoxes));
+    addHoverListener();
+});
+
+
+function deleteBoxes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+drawBoxes(676, 24);
+addHoverListener();
 
